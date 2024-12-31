519e6ced343bd5748459d862bdb522a2bf2750d8
Esteban Glochon <esteban.glochon@usach.cl>
2024-12-31 15:17:41 -0300

    fix key generation in encrypt, remove padding from last decrypted block

--- feistel_glochon.c
+++ feistel_glochon.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <string.h>
+
+#define round_t uint8_t
 
 // Constants
-const size_t BLOCK_SIZE = 128;
-const size_t HALF_BLOCK_SIZE = BLOCK_SIZE / 2;
+const size_t BLOCK_SIZE = 128UL;
+const size_t HALF_BLOCK_SIZE = BLOCK_SIZE / 2UL;
 const uint8_t INITIAL_KEY = 0b10010110;
 
 size_t get_total_blocks(FILE *fp);
 
 uint8_t circular_left_shift(uint8_t byte);
-uint8_t generate_key(uint8_t base_key, int rounds);
+uint8_t generate_key(uint8_t base_key, round_t rounds);
 
 uint8_t *apply_key_to_right_side(uint8_t *bytes, size_t size, uint8_t key);
 uint8_t *apply_xor_to_left_side(uint8_t *lbytes, uint8_t *rbytes, uint8_t size);
@@ -19,10 +22,12 @@ uint8_t *apply_xor_to_left_side(uint8_t *lbytes, uint8_t *rbytes, uint8_t size);
 uint8_t *clone_buffer(uint8_t *bytes, uint8_t size);
 void copy_buffer(uint8_t *buffer, uint8_t *bytes, uint8_t size);
 
-void init_encrypt(int rounds, const char *input_file, const char *output_file);
-void init_decrypt(int rounds, const char *input_file, const char *output_file);
-void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key, int rounds);
-void feistel_decrypt(uint8_t *block, uint8_t *output, uint8_t key, int rounds);
+void init_encrypt(round_t rounds, const char *input_file, const char *output_file);
+void init_decrypt(round_t rounds, const char *input_file, const char *output_file);
+void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key, round_t rounds);
+void feistel_decrypt(uint8_t *block, uint8_t *output, uint8_t key, round_t rounds);
+
+// void print_bytes(uint8_t *buffer, size_t length);
 
 int main(int argc, char *argv[]) {
 	if (argc != 5) {
@@ -31,7 +36,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	char mode = argv[1][0];
-	int rounds = atoi(argv[2]);
+	round_t rounds = (round_t)atoi(argv[2]);
 	const char *input_file = argv[3];
 	const char *output_file = argv[4];
 
@@ -56,7 +61,7 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-void init_encrypt(int rounds, const char *input_file, const char *output_file) {
+void init_encrypt(round_t rounds, const char *input_file, const char *output_file) {
 	FILE *input = fopen(input_file, "rb");
 	FILE *output = fopen(output_file, "wb");
 
@@ -74,13 +79,20 @@ void init_encrypt(int rounds, const char *input_file, const char *output_file) {
 
 	uint8_t *buffer;
 	uint8_t *encrypted;
+	size_t buffer_size = (BLOCK_SIZE * sizeof(uint8_t));
+	size_t bytes_read = 0;
 
 	for(size_t i = 0; i < blocks; ++i){
-		buffer = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t));
-		encrypted = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t));
-		if(fread(buffer, sizeof(uint8_t), BLOCK_SIZE, input) > 0){
+		buffer = (uint8_t*)malloc(buffer_size);
+		encrypted = (uint8_t*)malloc(buffer_size);
+		bytes_read = fread(buffer, sizeof(uint8_t), buffer_size, input);
+		if(bytes_read > 0){
+			if(bytes_read < buffer_size){
+				memset(buffer + bytes_read, 0, buffer_size - bytes_read);
+			}
 			feistel_encrypt(buffer, encrypted, INITIAL_KEY, rounds);
-			fwrite(encrypted, sizeof(uint8_t), BLOCK_SIZE, output);
+			// print_bytes(encrypted, buffer_size);
+			fwrite(encrypted, sizeof(uint8_t), buffer_size, output);
 		}
 		free(buffer);
 		free(encrypted);
@@ -89,7 +101,7 @@ void init_encrypt(int rounds, const char *input_file, const char *output_file) {
 	fclose(output);
 }
 
-void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key_0, int rounds) {
+void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key_0, round_t rounds) {
 	size_t buffer_size = (HALF_BLOCK_SIZE * sizeof(uint8_t));
 	uint8_t *left_prev = clone_buffer(block, buffer_size);
 	uint8_t *right_prev = clone_buffer(block + buffer_size, buffer_size);
@@ -97,8 +109,8 @@ void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key_0, int rounds)
 	uint8_t *right_curr = NULL;
 	uint8_t key_curr = key_0;
 
-	for(int i = 0; i < rounds; ++i){
-		key_curr = generate_key(key_curr, i);
+	for(round_t i = 0; i < rounds; ++i){
+		key_curr = circular_left_shift(key_curr);
 		right_curr = apply_key_to_right_side(right_prev, buffer_size, key_curr);
 		left_curr = apply_xor_to_left_side(left_prev, right_curr, buffer_size);
 		free(right_curr);
@@ -115,7 +127,7 @@ void feistel_encrypt(uint8_t *block, uint8_t *output, uint8_t key_0, int rounds)
 	free(right_prev);
 }
 
-void init_decrypt(int rounds, const char *input_file, const char *output_file) {
+void init_decrypt(round_t rounds, const char *input_file, const char *output_file) {
 	FILE *input = fopen(input_file, "rb");
 	FILE *output = fopen(output_file, "wb");
 
@@ -132,23 +144,37 @@ void init_decrypt(int rounds, const char *input_file, const char *output_file) {
 	size_t blocks = get_total_blocks(input);
 
 	uint8_t *buffer;
-	uint8_t *encrypted;
+	uint8_t *decrypted;
+	size_t buffer_size = (BLOCK_SIZE * sizeof(uint8_t));
+	size_t bytes_read = 0;
+	size_t real_size = BLOCK_SIZE;
 
 	for(size_t i = 0; i < blocks; ++i){
-		buffer = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t));
-		encrypted = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t));
-		if(fread(buffer, sizeof(uint8_t), BLOCK_SIZE, input) > 0){
-			feistel_decrypt(buffer, encrypted, INITIAL_KEY, rounds);
-			fwrite(encrypted, sizeof(uint8_t), BLOCK_SIZE, output);
+		buffer = (uint8_t*)malloc(buffer_size);
+		decrypted = (uint8_t*)malloc(buffer_size);
+		bytes_read = fread(buffer, sizeof(uint8_t), buffer_size, input);
+		if(bytes_read == buffer_size){
+			feistel_decrypt(buffer, decrypted, INITIAL_KEY, rounds);
+			if(i == (blocks - 1)){
+				while (real_size > 0 && decrypted[real_size - 1] == 0) --real_size;
+				fwrite(decrypted, sizeof(uint8_t), real_size * sizeof(uint8_t), output);
+			}
+			else{
+				fwrite(decrypted, sizeof(uint8_t), buffer_size, output);
+			}
+		}
+		else{
+			fprintf(stderr, "Invalid block size: %lu, expected: %lu", bytes_read, buffer_size);
+			exit(EXIT_FAILURE);
 		}
 		free(buffer);
-		free(encrypted);
+		free(decrypted);
 	}
 	fclose(input);
 	fclose(output);
 }
 
-void feistel_decrypt(uint8_t *block, uint8_t *output, uint8_t key_0, int rounds) {
+void feistel_decrypt(uint8_t *block, uint8_t *output, uint8_t key_0, round_t rounds) {
 	size_t buffer_size = (HALF_BLOCK_SIZE * sizeof(uint8_t));
 	uint8_t *left_prev = clone_buffer(block, buffer_size);
 	uint8_t *right_prev = clone_buffer(block + buffer_size, buffer_size);
@@ -156,8 +182,8 @@ void feistel_decrypt(uint8_t *block, uint8_t *output, uint8_t key_0, int rounds)
 	uint8_t *right_curr = NULL;
 	uint8_t key_curr = key_0;
 
-	for(int i = 0; i < rounds; ++i){
-		key_curr = generate_key(key_curr, rounds - i);
+	for(round_t i = 0; i < rounds; ++i){
+		key_curr = generate_key(key_0, rounds - i);
 		right_curr = apply_key_to_right_side(right_prev, buffer_size, key_curr);
 		left_curr = apply_xor_to_left_side(left_prev, right_curr, buffer_size);
 		free(right_curr);
@@ -189,14 +215,14 @@ uint8_t circular_left_shift(uint8_t byte) {
 	return (byte << 1) | (byte >> 7);
 }
 
-uint8_t generate_key(uint8_t base_key, int rounds){
+uint8_t generate_key(uint8_t base_key, round_t rounds){
 	uint8_t key = base_key;
-	for(int i = 0; i < rounds; ++i) key = circular_left_shift(key);
+	for(round_t i = 0; i < rounds; ++i) key = circular_left_shift(key);
 	return key;
 };
 
 uint8_t *apply_key_to_right_side(uint8_t *bytes, size_t size, uint8_t key) {
-	uint8_t *buffer = (uint8_t *)calloc(size, sizeof(uint8_t));
+	uint8_t *buffer = (uint8_t *)malloc(size * sizeof(uint8_t));
 	if (buffer == NULL) {
 		perror("Memory allocation failed");
 		exit(EXIT_FAILURE);
@@ -210,7 +236,7 @@ uint8_t *apply_key_to_right_side(uint8_t *bytes, size_t size, uint8_t key) {
 }
 
 uint8_t *apply_xor_to_left_side(uint8_t *lbytes, uint8_t *rbytes, uint8_t size) {
-	uint8_t *buffer = (uint8_t *)calloc(size, sizeof(uint8_t));
+	uint8_t *buffer = (uint8_t *)malloc(size * sizeof(uint8_t));
 	if (buffer == NULL) {
 		perror("Memory allocation failed");
 		exit(EXIT_FAILURE);
@@ -242,3 +268,16 @@ void copy_buffer(uint8_t *buffer, uint8_t *bytes, uint8_t size) {
 		buffer[i] = bytes[i];
 	}
 }
+
+// void print_bytes(uint8_t *buffer, size_t length){
+// 	uint8_t _char;
+// 	for(size_t i = 0; i < length; ++i){
+// 		_char = buffer[i];
+// 		if(_char >= 32 && _char <= 125){
+// 			printf("[%03lu] 0b%02hhx %03d %c\n", i, _char, (int)_char, _char);
+// 		}
+// 		else{
+// 			printf("[%03lu] 0b%02hhx %03d\n", i, _char, (int)_char);
+// 		}
+// 	}
+// }
